63b9a603ffb9de9c9426cf37384719de7e81a8d1
Beeerrt <b.geerken@web.de>
2018-03-10 19:44:21 +0100

    Favicon

--- MC_Projekt/src/java/servlet/dashboard.java
+++ MC_Projekt/src/java/servlet/dashboard.java
@@ -156,6 +156,7 @@ public class dashboard extends HttpServlet {
         String htmlHead = "<head>\n"
                 + "  <meta charset=\"utf-8\" />\n"
                 + "  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />\n"
+                + "<link rel=\"shortcut icon\" type=\"image/x-icon\" href=\"assets/fav.ico\">"
                 + "  <link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css\" integrity=\"sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy\"\n"
                 + "    crossorigin=\"anonymous\">\n"
                 + "  <script src=\"https://code.jquery.com/jquery-3.2.1.slim.min.js\" integrity=\"sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN\"\n"
